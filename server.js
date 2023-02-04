0b0a0274c6b2597b4f82df14852fcd25ef51c110
hari <harikn77@gmail.com>
2023-02-05 03:06:16 +0530

    Basic elasticsearch indexing and search

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
-// Create an API to interact with minio server
 const express = require('express');
+const { Client } = require('elasticsearch');
+const fs = require('fs');
 const app = express();
 const Minio = require('minio');
-const ACCESS_KEY = 'minio';
-const SECRET_KEY = 'miniostorage'
+
+const MINIO_ACCESS_KEY = 'minio';
+const MINIO_SECRET_KEY = 'miniostorage'
+const MINIO_ENDPOINT = '192.168.1.189';
+const MINIO_PORT = 9000;
+const MINIO_USE_SSL = false;
+
+const ELASTICSEARCH_HOST = 'http://192.168.1.189:9200';
+
+const index_name = 'testindex123';
+const bucketName = 'testpath123';
 
 const minioClient = new Minio.Client({
-    endPoint: 'localhost',
-    port: 9000,
-    useSSL: false,
-    accessKey: ACCESS_KEY,
-    secretKey: SECRET_KEY
+    endPoint: MINIO_ENDPOINT,
+    port: MINIO_PORT,
+    useSSL: MINIO_USE_SSL,
+    accessKey: MINIO_ACCESS_KEY,
+    secretKey: MINIO_SECRET_KEY
 })
+
+const elasticsearchClient = new Client({
+    host: ELASTICSEARCH_HOST
+});
+
 app.get('/', (req, res) => {
     res.send('minio sample app');
 })
 // Create a bucket
-const bucketName = 'test123';
 app.get('/createBucket', (req, res) => {
     minioClient.makeBucket(bucketName, 'us-east-1', function (err) {
         if (err) return console.log(err)
@@ -24,40 +38,92 @@ app.get('/createBucket', (req, res) => {
     });
     res.send('Bucket created successfully');
 })
-// List all buckets
-app.get('/listBuckets', (req, res) => {
-    minioClient.listBuckets(function (err, buckets) {
-        if (err) return console.log(err)
-        console.log('buckets : ', buckets)
-    });
-    res.send('Listed all buckets');
-})
-// on hitting localhost:3000/upload. open metadata.json and iterate over the files and upload them to minio server
-app.get('/upload', (req, res) => {
-    const fs = require('fs');
-    fs.readFile('./metadata.json', 'utf8', (error, data) => {
-        if (error) {
-            console.log(error);
-            return;
-        }
+
+app.get('/upload', async (req, res) => {
+    const indexing_data = [];
+    try {
+        const data = await fs.promises.readFile('./metadata.json', 'utf8');
         const files = JSON.parse(data);
         console.log('files : ', files);
-        files.forEach(file => {
+        for (const file of files) {
             const fileStream = fs.createReadStream(file.path);
-            const metaData = {
-                'Content-Type': 'application/octet-stream',
-                'nameX': file.name,
-                'age': file.age
+            const etag = await new Promise((resolve, reject) => {
+                minioClient.putObject(bucketName, file.path, fileStream, (err, etag) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(etag);
+                    }
+                });
+            });
+            console.log('etag : ', etag);
+            const doc = {
+                bucketName: bucketName,
+                objectID: file.path,
+                etag: etag,
+                name: file.name,
+                age: file.age
+            };
+            indexing_data.push(doc);
+        }
+        await fs.promises.writeFile('./indexing_data.json', JSON.stringify(indexing_data), 'utf8');
+        res.send('Uploaded files to minio server and wrote indexing data to file');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('An error occurred while uploading files to minio and writing indexing data to file');
+    }
+});
+
+app.get('/index', async (req, res) => {
+    try {
+        const data = await fs.promises.readFile('./indexing_data.json', 'utf8');
+        const indexing_data = JSON.parse(data);
+        console.log('indexing_data : ', indexing_data);
+
+        const body = indexing_data.reduce((bulkRequestBody, doc) => {
+            bulkRequestBody += JSON.stringify({ index: { _index: index_name } }) + '\n';
+            bulkRequestBody += JSON.stringify(doc) + '\n';
+            return bulkRequestBody;
+        }, '');
+
+        const response = await elasticsearchClient.bulk({
+            body: body
+        });
+
+        console.log('response : ', response);
+        res.send('Indexed data in elasticsearch');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('An error occurred while indexing data in elasticsearch');
+    }
+});
+
+app.get('/search/:name', async (req, res) => {
+    const name = req.params.name;
+    console.log('name : ', name);
+    try {
+        const response = await elasticsearchClient.search({
+            index: index_name,
+            body: {
+                query: {
+                    match: {
+                        name: name
+                    }
+                }
             }
-            minioClient.putObject(bucketName, file.name, fileStream, metaData, (err, etag) => console.log(err, etag) // err should be null
-            );
-        })
-    })
-    res.send('Uploaded files to minio server');
-})
+        });
+        console.log('response : ', response);
+        const hits = response.hits.hits;
+        const data = hits.map(hit => hit._source);
+        const object_ids = data.map(d => d.objectID);
+        res.send('Bucket name : ' + data[0].bucketName + ' Object ids : ' + object_ids);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('An error occurred while searching data in elasticsearch');
+    }
+});
+
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 })
-
-
-
